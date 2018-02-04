dfa42c6606599ead4386976e2eaaa0f2fef7b85a
Kravtsov Roman <rozario91208@gmail.com>
2018-02-04 22:51:27 +0200

    prod

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,7 +3,7 @@ document.body.onload = function () {
 	
 	//Enter to page
 	setTimeout(function(){
-		var preloader = document.getElementById('preloader');
+		let preloader = document.getElementById('preloader');
 		if(!preloader.classList.contains('done') ){
 			preloader.classList.add('done');
 		}
@@ -11,10 +11,19 @@ document.body.onload = function () {
 
 	// Scroll page
 	window.onscroll = function() {
-	  var scrolled = window.pageYOffset ||  
-	  				 document.documentElement.scrollTop,
-	  	   	block =  document.getElementsByClassName('offer-container')[0];
-  
+	  let scrolled = window.pageYOffset || document.documentElement.scrollTop,
+	  	  block =  document.getElementsByClassName('offer-container')[0],
+	  	  header =  document.getElementsByClassName('header')[0];
+  		
+  		// Change header background	
+	  	if (scrolled >= 400) {
+	  		header.classList.remove('header-bg-top');
+	  		header.classList.add('header-bg-middle');
+	  	}else if (scrolled < 400){
+	  		header.classList.remove('header-bg-middle');
+	  		header.classList.add('header-bg-top');
+	  	}
+	  	// Animation box
   		if (block.scrollTop + 150 < scrolled) {
   			block.classList.add('box-our-offer__fade-out');
   			setTimeout(function(){
