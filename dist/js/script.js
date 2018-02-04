2e593038a9a2ce33014b99397577775cc62c753e
Kravtsov Roman <rozario91208@gmail.com>
2018-02-04 19:43:31 +0200

    animation (enter + scroll)

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,9 +1,25 @@
+
 document.body.onload = function () {
 	
+	//Enter to page
 	setTimeout(function(){
 		var preloader = document.getElementById('preloader');
 		if(!preloader.classList.contains('done') ){
 			preloader.classList.add('done');
 		}
 	}, 2000);
+
+	// Scroll page
+	window.onscroll = function() {
+	  var scrolled = window.pageYOffset ||  
+	  				 document.documentElement.scrollTop,
+	  	   	block =  document.getElementsByClassName('offer-container')[0];
+  
+  		if (block.scrollTop + 150 < scrolled) {
+  			block.classList.add('box-our-offer__fade-out');
+  			setTimeout(function(){
+  				block.classList.add('right');
+  			}, 1000);		
+	  	}
+	}
 }
